a71392ea27700f4789d599de830bc30ce8bef972
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-07-11 21:58:03 +0000

    Update tests/test_base_model.py add more tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -130,7 +130,30 @@ class TestBaseModel(unittest.TestCase):
         model_json = model.to_dict()
         new_model = BaseModel(**model_json)
         self.assertEqual(new_model.name, model.name)
-        self.assertEqual(new_model.my_number, model.number)
+        self.assertEqual(new_model.my_number, model.my_number)
+        self.assertEqual(new_model.created_at, model.created_aat)
+        self.assertEqual(new_model.updated_at, model.updated_at)
+
+    def test_unique_id(self):
+        """
+        Test the uniqueness of the 'id' attribute.
+
+        This test ensures that each instance of the BaseModel class has
+        a unique 'id' value.
+        """
+        model1 = BaseModel()
+        model2 = BaseModel()
+        self.assertNotEqual(model1.id, model2.id)
+
+    def test_created_at_type(self):
+        """
+        Test the type of the 'created_at' attribute.
+
+        This test ensures that the 'created_at' attribute is of type
+        'datetime'.
+        """
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
 
 
 if __name__ == '__main__':
