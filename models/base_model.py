1347b5f66cb258b03908b02bfdeee79b71956551
MohdMuslim92 <mohammed.muslim2022@gmail.com>
2023-07-11 20:42:11 +0000

    Update models/base_model.py add attributes/methods
    - Add __str__: which print string representation for the BaseModel:
    [<class name>] (<self.id>) <self.__dict__>
    - Add Public instance methods:
        - save(self): updates the public instance attribute updated_at with the
        current datetime
        - to_dict(self): returns a dictionary containing all keys/values of
        __dict__ of the instance
    
    - Update created_at and updated_add by using datetime.now() instead of
    datetime.today() which has the ability to pass an optional timezone argument.

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,32 @@ defines all common attributes/methods for other classes
 """
 
 from uuid import uuid4
+from datetime import datetime
+
 
 class BaseModel():
+    """A base model class for AirBnB objects."""
+
     def __init__(self):
         """initialize attributes"""
 
         self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
