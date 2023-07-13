cb866a3399cad19fba3727750f5eb463e5063ff4
Reem-degais <reem.degais.33@gmail.com>
2023-07-13 10:14:37 -0700

    -

--- console.py
+++ console.py
@@ -20,14 +20,21 @@ Usage:
 import cmd
 from models.base_model import BaseModel
 from models import storage
-
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for the HBNB program."""
     prompt = "(hbnb) "
 
     __classes = {
-        "BaseModel"}
+        "BaseModel", "User", "State",
+        "City", "Place", "Amenity",
+        "Review"}
 
     def do_quit(self, arg):
         """Exit the program.
