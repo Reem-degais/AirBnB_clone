64376172adfbe36f271e9bdb51d16a22042c8545
Reem-degais <reem.degais.33@gmail.com>
2023-07-15 09:40:27 -0700

    -

--- models/place.py
+++ models/place.py
@@ -20,15 +20,14 @@ class Place(BaseModel):
         amenity_ids : A list of Amenity ids.
     """
 
-
-city_id = ""
-user_id = ""
-name = ""
-description = ""
-number_rooms = 0
-number_bathrooms = 0
-max_guest = 0
-price_by_night = 0
-latitude = 0.0
-longitude = 0.0
-amenity_ids = []
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
