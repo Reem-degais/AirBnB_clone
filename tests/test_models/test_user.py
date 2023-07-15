45e203c47b804ad14d914a1fa1195696a24dd815
Reem-degais <reem.degais.33@gmail.com>
2023-07-15 07:21:41 -0700

    -

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Defines unittests for user class"""
 
 import models
 import unittest
 from datetime import datetime
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """Unittests for testing User class"""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(User, type(User()))
+
+if __name__ == "__main__":
+    unittest.main()
