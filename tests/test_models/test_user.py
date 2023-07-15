eeaeff5dcdee3bed9857940d0d05093b4fb71041
Reem-degais <reem.degais.33@gmail.com>
2023-07-15 07:34:23 -0700

    -

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -14,5 +14,26 @@ class TestUser(unittest.TestCase):
     def test_no_args_instantiates(self):
         self.assertEqual(User, type(User()))
 
+     def test_id_is_str(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at_is_datetime(self):
+        self.assertEqual(datetime, type(User().created_at))
+
+    def test_updated_at_is_datetime(self):
+        self.assertEqual(datetime, type(User().updated_at))
+
+    def test_email_is_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_str(self):
+        self.assertEqual(str, type(User.last_name))
+
 if __name__ == "__main__":
     unittest.main()
